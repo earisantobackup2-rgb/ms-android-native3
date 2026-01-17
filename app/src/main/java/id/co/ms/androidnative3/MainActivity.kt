5d2ebe677a3d933b496b84cab70ba85e5f2cd4a5
earisantobackup2-rgb <earisantobackup2@gmail.com>
2026-01-17 22:05:55 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/id/co/ms/androidnative3/MainActivity.kt
@@ -0,0 +1,18 @@
+package id.co.ms.androidnative3
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val tv = TextView(this)
+        tv.text = "MS Android Native 3\nBuild via GitHub Actions OK"
+        tv.textSize = 18f
+
+        setContentView(tv)
+    }
+}
